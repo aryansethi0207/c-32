38c0003b9418395b760c1e790207a79ae828b788
@aryansethi0207 <aryansethi0207@gmail.com>
2021-02-12 20:28:36 +0530

    added bgImage acc time

--- sketch.js
+++ sketch.js
@@ -5,13 +5,14 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1,pig3;
-var backgroundImg,platform;
+var bgImage,platform;
 var bird, slingshot;
-
+var score=0;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+  
+    getTime();
 }
 
 function setup(){
@@ -26,12 +27,12 @@ function setup(){
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+  log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
+   
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
@@ -45,18 +46,28 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+   if(bgImage){
+background(bgImage);
+
+   }
+   textSize(35);
+   fill("white");
+   text("score:"+score,800,50)
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
+   
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +96,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+async function getTime(){
+var response=await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata");
+var responseJSON=await response.json();
+var datetime=responseJSON.datetime;
+var hour=datetime.slice(11,13);
+if(hour>=06&&hour<=19){
+bg="sprites/bg.png"
+
+}
+else(
+bg="sprites/bg2.jpg"
+)
+bgImage=loadImage(bg);
+
 }
